50a2fb6ee9c4877989fc7115921b03fb5fcef145
01 <159622426+OandONE@users.noreply.github.com>
2025-10-12 16:40:36 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PySPlus",
-    version="0.5",
+    version="0.6",
     author="seyyed mohamad hosein moosavi raja(01)",
     author_email="mohamadhosein159159@gmail.com",
     description="the library SPlus platform for bots.",
@@ -11,6 +11,6 @@ setup(
     url="https://github.com/OandONE/SPlus",
     packages=find_packages(),
     python_requires='>=3.8',
-    install_requires=["selenium==4.29.0","webdriver_manager==4.0.2","bs4==0.0.2"],
+    install_requires=["selenium==4.29.0","webdriver_manager==4.0.2","bs4==0.0.2","pytz"],
     license="MIT"
 )
